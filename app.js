2e774feb57eb3299dec1fcb17195dd513f3a5ced
lucian.oprea <lucian.oprea@metrosystems.net>
2019-01-02 15:20:56 +0200

    interactive list

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
-//Define UI vars
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector(".collection");
-const clearBTN = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
-const taskInput = document.querySelector("#task");
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-//Load all event listeners
+// Load all event listeners
 loadEventListeners();
 
+// Load all event listeners
 function loadEventListeners() {
-    //Add task event
+    // Add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear tasks
+    clearBtn.addEventListener('click', clearTasks);
+
+    filter.addEventListener('keyup', filterTasks);
 }
 
+// Add Task
 function addTask(e) {
     if (taskInput.value === '') {
-        alert("Add a task");
+        alert('Add a task');
     }
 
-    //Create li element
+    // Create li element
     const li = document.createElement('li');
+    // Add class
     li.className = 'collection-item';
-    //Create a text node and append to li
+    // Create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
-    //Create new link element
+    // Create new link element
     const link = document.createElement('a');
-    link.className = 'delete-item secondary content';
-    //Add icon html
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
     link.innerHTML = '<i class="fa fa-remove"></i>';
-    //Append the link to the li
+    // Append the link to li
     li.appendChild(link);
 
-    //Append the li to the ul
-    console.log(li);
+    // Append li to ul
     taskList.appendChild(li);
 
-    //Clear input
+    // Clear input
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+//Remove task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearTasks() {
+    // taskList.innerHTML = '';
+    //Faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item')
+        .forEach(function (task) {
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) !== -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        })
 }
